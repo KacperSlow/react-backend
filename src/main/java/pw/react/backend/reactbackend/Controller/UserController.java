8b3eef7df7413a05591a05d216797c8ce078505e
KacperSlow <k.slowikowski@student.mini.pw.edu.pl>
2019-11-02 21:08:20 +0100

    Lab2Ex3

--- src/main/java/pw/react/backend/reactbackend/Controller/UserController.java
+++ src/main/java/pw/react/backend/reactbackend/Controller/UserController.java
@@ -35,4 +35,46 @@ public class UserController {
         }
         return "User: " + user.getLogin() + ", " + user.getFirstName()+ ", " + user.getLastName() + ", "+ user.getActive();
     }
+    @DeleteMapping("/{login}")
+    public String delete(@PathVariable String login, HttpServletResponse respon){
+        User user = repository.findByLogin(login);
+        if(user == null)
+        {
+            response = respon;
+            response.setStatus(404);
+            return String.format("User with login: [%s] does not exist",login);
+        }
+        repository.delete(user);
+        return String.format("User with login: [%s] was deleted", user.getLogin());
+    }
+    @PutMapping("/{login}")
+    public String update(@PathVariable String login,@RequestBody User newuser, HttpServletResponse respon) {
+        User user = repository.findByLogin(login);
+        if (user == null) {
+            response = respon;
+            response.setStatus(404);
+            return String.format("User with login: [%s] does not exist", login);
+        }
+        if (login.equals(newuser.getLogin())) {
+            user.setActive(newuser.getActive());
+            user.setFirstName(newuser.getFirstName());
+            user.setLastName(newuser.getLastName());
+            repository.save(user);
+            return String.format("User with login: [%s] was updated", user.getLogin());
+        }
+        response = respon;
+        response.setStatus(304);
+        return String.format("User with login: [%s] does not update - not matching to [%s]", login, newuser.getLogin());
+    }
+    @GetMapping("/retrive/{login}")
+    public <T> T retrive(@PathVariable String login, HttpServletResponse respon){
+        User user = repository.findByLogin(login);
+        if (user == null) {
+            response = respon;
+            response.setStatus(404);
+            return (T) (String)"User with login: [%s] does not exist";
+        }
+        return (T) user;
+    }
+
 }
